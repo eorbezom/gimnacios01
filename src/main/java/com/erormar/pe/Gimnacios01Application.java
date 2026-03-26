5c2fba7dbc3d39b617812d466402e869f0cd7bf1
eorbezom <eorbezo99@gmail.com>
2026-03-25 22:44:49 -0500

    proyecto sin modificar nada

---
+++ src/main/java/com/erormar/pe/Gimnacios01Application.java
@@ -0,0 +1,13 @@
+package com.erormar.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gimnacios01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gimnacios01Application.class, args);
+	}
+
+}

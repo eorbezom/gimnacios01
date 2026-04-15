e7a0e97d0235a000b89c9db3eefec4c0c18ee84b
eorbezom <eorbezo99@gmail.com>
2026-04-14 19:13:35 -0500

    se agrega  cotegorias  funcionando y captado

---
+++ src/main/java/com/erormar/pe/controller/CategoriaController.java
@@ -0,0 +1,34 @@
+package com.erormar.pe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value="/categorias")
+public class CategoriaController {
+	
+	
+	@RequestMapping(value="/index", method = RequestMethod.GET)
+	public String mostrarIndex(Model model) {
+		return"categorias/listCategorias";
+	}
+	
+	@RequestMapping(value="/create", method = RequestMethod.GET)
+	public String crear() {
+		return"categorias/formCategorias";
+	}
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion")String descripcion ) {
+		System.out.println("categorias :"+nombre);
+		System.out.println("descripcion : "+descripcion);
+		return"categorias/listCategorias";
+	}
+	
+	
+
+}
--- src/main/java/com/erormar/pe/controller/DetalleController.java
+++ src/main/java/com/erormar/pe/controller/DetalleController.java
@@ -5,11 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/persona")
 public class DetalleController {
 	
+	
+	@GetMapping("/delete")
+	public String borrar(@RequestParam("id") int idPersona, Model model) {
+		System.out.println("Se eliminoó el id: "+idPersona);
+		model.addAttribute("id", idPersona);
+		
+		return"mensaje";
+	}
 	
 	@GetMapping("/view/{id}")
 	public String verPersona(@PathVariable("id") int IdPersona, Model model) {
